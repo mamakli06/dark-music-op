7eaf8afa2552e60b341ac7f5337e25f8735ad0a2
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-09-10 13:51:17 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -22,24 +22,9 @@ async def start(_, message: Message):
 ⚜️ /yt (song name) - To search song from youtube and play directly 
         """,
         reply_markup=InlineKeyboardMarkup(
-            [
-                 [
-                    InlineKeyboardButton(
-                        "Group 💬", url="https://t.me/phoenix_music_suport"),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "Channel 📣", url="https://t.me/phoenix_music_new"),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "Owner 👑", url="https://t.me/akshi_s_ashu"),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "Commands", url="https://telegra.ph/Doreamon-Bot-09-10"),   
-                ],
-          ]
+           keyb = [[InlineKeyboardButton("Group 💬", url="https://t.me/phoenix_music_suport"),InlineKeyboardButton("Channel 📣", url="https://t.me/phoenix_music_new")]]
+keyb += [[InlineKeyboardButton("Owner 👑", url="https://t.me/akshi_s_ashu"),InlineKeyboardButton("Commands", url="https://telegra.ph/Doreamon-Bot-09-10" )]]
+
 
 from pyrogram import Client, filters
 
